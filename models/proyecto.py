58f77eda649976e002cf9032dff54e64a2d5b9df
MT23002 <mt23002@ues.edu.sv>
2026-06-07 18:44:02 -0600

    cambios solicitados a clases

--- models/proyecto.py
+++ models/proyecto.py
@@ -1,5 +1,6 @@
-#clase para crear los proyectos, con las validaciones para id, nomre y estado
 class Proyecto:
+  """clase para crear los proyectos, con las validaciones para id, nomre y estado"""
+
   def __init__(self, id, nombre, estado="Activo"):
     if not isinstance(id, int) or id <= 0:
       raise ValueError("El ID del proyecto debe ser un entero positivo.")
@@ -13,21 +14,27 @@ class Proyecto:
     self.id = id
     self.nombre = nombre.strip()
     self.estado = estado
-#metodo para guardar los datos en JSON
+
   def to_dict(self):
+    """metodo para guardar los datos en JSON"""
+
     return {
       "id": self.id,
       "nombre": self.nombre,
       "estado": self.estado,
     }
-#metodo de clase para traer un proyecto
+
   @classmethod
   def from_dict(cls, data):
+    """metodo de clase para traer un proyecto"""
+
     return cls(
       id=data["id"],
       nombre=data["nombre"],
       estado=data.get("estado", "Activo"),
     )
-#metodo para imprimir los atributos de un proyecto 
+ 
   def __str__(self):
+    """metodo para imprimir los atributos de un proyecto"""
+    
     return f"Proyecto(id={self.id}, nombre='{self.nombre}', estado='{self.estado}')"
